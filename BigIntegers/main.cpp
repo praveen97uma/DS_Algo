69a4ed2794693f69d513326646beda112428823c
Praveen <praveen97uma@gmail.com>
2011-03-01 02:27:52 +0530

    addition of ints finishd, multiplication yet to be implemented

--- BigIntegers/main.cpp
+++ BigIntegers/main.cpp
@@ -5,67 +5,183 @@
 #include<cstdlib>
 using namespace std;
 
+
+
 class BigInt
 {
     friend ostream &operator<<(ostream &, const BigInt &);
 
-    //char num[100];
 
-    void addTwoNum();
     public:
     BigInt(){len_str=0;}
     int len_str;
-    BigInt(char* s);
+    BigInt(string s);
+    BigInt operator+(const BigInt& ) const;
+    BigInt operator*(const int&);
     vector<int> intRep;
-    //void display();
+
 
 };
 
+BigInt addTwoBigNum(BigInt&, BigInt&);
 
-void addTwoBigNum(BigInt* num1, BigInt* num2)
+BigInt BigInt::operator*(const int& nu )
 {
-    int carry=0;
+    BigInt* res,temp;
+
+    *res=*this;
+/*
+    for(int i=1;i<nu;i++)
+    {
+        *result=addTwoBigNum(*this,*result);
+    }
+*/
+//temp=addTwoBigNum(*this,*this);
+*res=temp;
+//temp=addTwoBigNum(*this,*this);
+*res=*res+temp;
+cout<<*res<<*this;
+    return *res;
+}
+
+BigInt BigInt::operator+(const BigInt& num2) const
+{
+     int carry=0;
     BigInt* result;
     result=new BigInt();
     int size1=0,size2=0;
-    if (num1->len_str>num2->len_str)
-    {
-        size1=num1->len_str;
-    }
 
-    else {size=num2->len_str;}
+        size1=len_str;
+        size2=num2.len_str;
 
-    int r;int s=size1-1;int q=0,u=0;
-    for(int i=0;i<size1;i++)
+
+    int r;size1=size1-1,size2=size2-1;int u=0;
+
+    while(size1>=0 && size2>=0)
     {
-        r=(num1->intRep[s])+(num2->intRep[s])+carry;
+        //cout<<"size1 "<<size1<<" "<<"size2 is "<<size2<<endl;
+        r=(intRep[size1])+(num2.intRep[size2])+carry;
         carry=r/10;
         u=r%10;
-        cout<<carry<<" "<<u<<endl;
+        //cout<<carry<<" "<<u<<endl;
         result->intRep.push_back(u);
-        s--;
+        size1--;size2--;
+    }
+
+    if (size1>0 )
+    {
+        //cout<<"first if"<<endl;
+        r=(intRep[size1])+carry;
+        int s=size1-1;
+        for(int i=0;i<size1;i++)
+        {
+            result->intRep.push_back(r);
+            //cout<<"r is "<<r<<endl;
+            r=(intRep[s]);
+            s--;
+        }
+    }
+    if(size2>0)
+    {
+        //cout<<"second if"<<endl;
+        r=(num2.intRep[size2])+carry;
+        int s=size2-1;
+        for(int i=0;i<size2;i++)
+        {
+            result->intRep.push_back(r);
+            //cout<<"r is "<<r<<endl;
+            r=(num2.intRep[s]);
+            s--;
+        }
+
     }
 
     if (carry!=0)
     {
         result->intRep.push_back(carry);
     }
-    cout<<*result;
+    return *result;
 
 }
 
-BigInt::BigInt(char* s)
+
+BigInt addTwoBigNum(BigInt& num1, BigInt& num2)
 {
-    len_str=0;
-    char* p=s;
+    int carry=0;
+    BigInt* result;
+    result=new BigInt();
+    int size1=0,size2=0;
+
+        size1=num1.len_str;
+        size2=num2.len_str;
+
+
+    int r;size1=size1-1,size2=size2-1;int u=0;
+
+    while(size1>=0 && size2>=0)
+    {
+        //cout<<"size1 "<<size1<<" "<<"size2 is "<<size2<<endl;
+        r=(num1.intRep[size1])+(num2.intRep[size2])+carry;
+        carry=r/10;
+        u=r%10;
+        //cout<<carry<<" "<<u<<endl;
+        result->intRep.push_back(u);
+        size1--;size2--;
+    }
+
+    if (size1>0 )
+    {
+        //cout<<"first if"<<endl;
+        r=(num1.intRep[size1])+carry;
+        int s=size1-1;
+        for(int i=0;i<size1;i++)
+        {
+            result->intRep.push_back(r);
+            //cout<<"r is "<<r<<endl;
+            r=(num1.intRep[s]);
+            s--;
+        }
+    }
+    if(size2>0)
+    {
+        //cout<<"second if"<<endl;
+        r=(num2.intRep[size2])+carry;
+        int s=size2-1;
+        for(int i=0;i<size2;i++)
+        {
+            result->intRep.push_back(r);
+            //cout<<"r is "<<r<<endl;
+            r=(num2.intRep[s]);
+            s--;
+        }
+
+    }
+
+    if (carry!=0)
+    {
+        result->intRep.push_back(carry);
+    }
+    return *result;
+
+}
+
+
+
+BigInt::BigInt(string s)
+{
+    len_str=s.length();
+    char* p=new char[len_str+1];
+    s.copy(p,len_str,0);
+    p[len_str]='\0';
     int c=0;
     //char* t=num;
     while (*p!='\0')
     {
         c=(int(*p)-48);
+        //cout<<"c "<<c<<" ";
         intRep.push_back(c);
         p++;
-        len_str++;
+        //len_str++;
 
     }
 
@@ -88,14 +204,14 @@ ostream& operator<<(ostream &output, const BigInt &num)
 
 int main()
 {
-    char b[]="992894528958324873298473298742937493284793287937237992894528958324873298473298742937493284793287937237992894528958324873298473298742937493284793287937237992894528958324873298473298742937493284793287937237992894528958324873298473298742937493284793287937237992894528958324873298473298742937493284793287937237992894528958324873298473298742937493284793287937237992894528958324873298473298742937493284793287937237992894528958324873298473298742937493284793287937237";
-        BigInt a(b) ;
+    //char b[]="123456789";
+        BigInt a("12345") ;
     cout<<a;
 
-    char n[]="992894528958324873298473298742937493284793287937237992894528958324873298473298742937493284793287937237992894528958324873298473298742937493284793287937237992894528958324873298473298742937493284793287937237992894528958324873298473298742937493284793287937237992894528958324873298473298742937493284793287937237992894528958324873298473298742937493284793287937237992894528958324873298473298742937493284793287937237992894528958324873298473298742937493284793287937237";
-    BigInt k(n);
-    cout<<k;
-    addTwoBigNum(&a,&k);
+    //char n[]="5";
+    BigInt k("5");
+    cout<<k*6;
+    //addTwoBigNum(&a,&k);
     return 0;
 
 }
